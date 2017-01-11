5fceac6c5b2467b7545c3052ee41dc5b7d48d160
Lukas Puehringer <lukas.puehringer@nyu.edu>
2017-01-11 12:05:26 -0500

    Adds more font and background color constants

--- uptane_banners.py
+++ uptane_banners.py
@@ -26,12 +26,20 @@ from subprocess import Popen, call, PIPE
 RED = "\033[31m"
 GREEN = "\033[32m"
 YELLOW = "\033[33m"
+BLUE = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+WHITE = "\033[97m"
 
 # Bash background color escape sequences
 BLACK_BG = "\033[40m"
 BLUE_BG = "\033[44m"
 WHITE_BG = "\033[107m"
 GRAY_BG = "\033[100m"
+RED_BG = "\033[41m"
+MAGENTA_BG = "\033[45m"
+GREEN_BG = "\033[42m"
+YELLOW_BG = "\033[43m"
 
 RESET_COLOR = '\033[0m'
 
@@ -186,9 +194,9 @@ cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidattttat non
 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
 
-  print_banner(BANNER_UPDATED, color=GREEN, color_bg=GRAY_BG, show_for=3, text=text)
-  print_banner(BANNER_DEFENDED, color=YELLOW, show_for=3, text=text)
-  print_banner(BANNER_FROZEN, color=YELLOW, color_bg=BLUE_BG, show_for=3, text=text)
+  print_banner(BANNER_UPDATED, color=GREEN, show_for=3, text=text)
+  print_banner(BANNER_DEFENDED, color=BLUE, color_bg=YELLOW_BG, show_for=3, text=text)
+  print_banner(BANNER_FROZEN, color=CYAN, color_bg=GRAY_BG, show_for=3, text=text)
   print_banner(BANNER_COMPROMISED, color=RED, color_bg=BLACK_BG, show_for=3, text=text)
   print_banner(BANNER_HACKED, color=RED, color_bg=BLACK_BG, show_for=3, text=text)
   print_banner(BANNER_REPLAY, color=RED, color_bg=BLACK_BG, show_for=3, text=text)
